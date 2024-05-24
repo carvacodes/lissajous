ec707009cb4bd419c4557f56295668c7a304debd
ac-scruffyk <acarva1@yahoo.com>
2024-05-24 11:31:37 -0500

    apply requestAnimationFrame call frequency adjustments to account for device frame rate

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.addEventListener('load', ()=>{
 // set up constants
 const _w = window.innerWidth;
 const _h = window.innerHeight;
@@ -22,55 +23,57 @@ let lissaDi = lissaSize - lissaPadding;
 let lissaRad = lissaDi / 2;
 let lissaTimer = 0;
 let lissas = [];
-let updateSpeed = (Math.PI / 300) / sizeRatio;
+let updateSpeed = (Math.PI / 250) / sizeRatio;    // the relative distance through the curve each point should advance per frame
+// updateSpeed is also adjusted in the animation loop based on the time between requestAnimationFrame calls,
+// which equalizes animation speed across multiple device frame rates
 
 // set up lissa prototype
-function Lissa(xFactor, yFactor) {
-  this.xFactor = xFactor;
-  this.yFactor = yFactor;
-  // the center of each lissa
-  this.origin = {
-    x: ((xFactor) * lissaSize) + lissaRad,
-    y: ((yFactor) * lissaSize) + lissaRad 
-  };
-  // the dot that will track the current position in the pattern
-  this.dot = {
-    x: this.origin.x + lissaRad,
-    y: this.origin.y,
-    lastX: this.origin.x + lissaRad,
-    lastY: this.origin.y
-  };
-  // color variables
-  this.rComp = 100 + (155 / (c.width / this.origin.x));
-  this.gComp = 100 + (155 / (((c.width / this.origin.x)) / (c.height / this.origin.y)));
-  this.bComp = 100 + (155 / (c.height / this.origin.y));
-  this.color = 'rgb(' + this.rComp + ',' + this.gComp + ',' + this.bComp + ')';
+class Lissa {
+  constructor(xFactor, yFactor) {
+    this.xFactor = xFactor;
+    this.yFactor = yFactor;
+    // the center of each lissa
+    this.origin = {
+      x: ((xFactor) * lissaSize) + lissaRad,
+      y: ((yFactor) * lissaSize) + lissaRad
+    };
+    // the dot that will track the current position in the pattern
+    this.dot = {
+      x: this.origin.x + lissaRad,
+      y: this.origin.y,
+      lastX: this.origin.x + lissaRad,
+      lastY: this.origin.y
+    };
+    // color variables
+    this.rComp = 100 + (155 / (c.width / this.origin.x));
+    this.gComp = 100 + (155 / (((c.width / this.origin.x)) / (c.height / this.origin.y)));
+    this.bComp = 100 + (155 / (c.height / this.origin.y));
+    this.color = 'rgb(' + this.rComp + ',' + this.gComp + ',' + this.bComp + ')';
+  }
+  // update the dot on each iteration
+  updateDot() {
+    this.dot.lastX = this.dot.x;
+    this.dot.lastY = this.dot.y;
+    this.dot.x = (lissaRad * Math.cos(lissaTimer * this.xFactor)) + this.origin.x;
+    this.dot.y = (lissaRad * Math.sin(lissaTimer * this.yFactor)) + this.origin.y;
+  }
+  // draw using the dot.lastX/Y and dot.x/y props
+  draw() {
+    let currentLissa = this;
+    ctx.lineWidth = lissaDotSize;
+    ctx.strokeStyle = currentLissa.color;
+    ctx.beginPath();
+    ctx.moveTo(currentLissa.dot.lastX, currentLissa.dot.lastY);
+    ctx.lineTo(currentLissa.dot.x, currentLissa.dot.y);
+    ctx.stroke();
+    ctx.closePath();
+    /*
+    using lineTo between the previous/current dot positions ended up being a more satisfying
+    effect than using the arc() drawing method, since the line you trace ends up being continuous.
+    with arc, at higher speeds, the dots begin to break apart, resulting in a more pixelated curve
+    */
+  }
 }
-
-// update the dot on each iteration
-Lissa.prototype.updateDot = function(){
-  this.dot.lastX = this.dot.x;
-  this.dot.lastY = this.dot.y;
-  this.dot.x = (lissaRad * Math.cos(lissaTimer * this.xFactor)) + this.origin.x;
-  this.dot.y = (lissaRad * Math.sin(lissaTimer * this.yFactor)) + this.origin.y;
-};
-
-// draw using the dot.lastX/Y and dot.x/y props
-Lissa.prototype.draw = function(){
-  let currentLissa = this;
-  ctx.lineWidth = lissaDotSize;
-  ctx.strokeStyle = currentLissa.color;
-  ctx.beginPath();
-  ctx.moveTo(currentLissa.dot.lastX, currentLissa.dot.lastY);
-  ctx.lineTo(currentLissa.dot.x, currentLissa.dot.y);
-  ctx.stroke();
-  ctx.closePath();
-  /*
-  using lineTo between the previous/current dot positions ended up being a more satisfying 
-  effect than using the arc() drawing method, since the line you trace ends up being continuous.
-  with arc, at higher speeds, the dots begin to break apart, resulting in a more pixelated curve
-  */
-};
 
 // clears the frame to a specified level of opacity, a float between 0 and 1, inclusive
 function clearFrame(opacity) {
@@ -83,21 +86,33 @@ function clearFrame(opacity) {
 };
 
 let ghostingFading = false;
-let lastFrameTime = Date.now();
+
+// these variables will adjust each curve's movement speed to match the frame rate of the device (which is the time between rAF calls).
+let firstFrameTime = performance.now();
+let timerCounter = 0;
+let refreshThrottle = 1;
+let tempRefreshThrottle = 0;
 
 // animation function
-function animate() {
-  // lissaTimer = lissaTimer >= 2 * Math.PI ? 0 : lissaTimer + (Math.PI / 360); // resets the timer to 0 once a rotation of 2pi is complete
-
-  // lock the animation to 60fps
-  let animTime = Date.now();
-  if (animTime - lastFrameTime <= 16) {
-    window.requestAnimationFrame(animate);
-    return;
+function animate(callbackTime) {
+  // gather movement speed vars for the first four valid frames
+  if (callbackTime) {   // callbackTime can be NaN on frame 2, so check it each time
+    // only gather data on the first four frames of animation
+    if (timerCounter < 5) {
+      timerCounter++;
+      tempRefreshThrottle += callbackTime - firstFrameTime;   // this is a sum, and will be averaged after four frames have elapsed
+    }
+    firstFrameTime = callbackTime;
   }
   
-  lastFrameTime = animTime;
+  // calculate the frame update adjustment by averaging the tempRefreshThrottle for these four frames
+  if (timerCounter == 4) {
+    tempRefreshThrottle /= 4;
+    refreshThrottle = 1 / tempRefreshThrottle;
+  }
   
+  // lissaTimer = lissaTimer >= 2 * Math.PI ? 0 : lissaTimer + (Math.PI / 360);
+  // reset the timer to 0 once a rotation of 2pi is complete
   if (lissaTimer >= 2 * Math.PI) {
     lissaTimer = 0;
     if (!ghostingFading) {
@@ -107,7 +122,7 @@ function animate() {
         fadeIntensity = 0;
     }
   } else {
-    lissaTimer += (updateSpeed);
+    lissaTimer += (updateSpeed * refreshThrottle);    // apply calculated refreshThrottle to the timer
     if (ghostingFading) {
         fadeIntensity += 0.0001;
     }
@@ -135,3 +150,4 @@ function init() {
 
 // begin!
 init();
+});
