73ddf016c26c9b9e4abb0cecc0979e1f893ed6b4
ac-scruffyk <acarva1@yahoo.com>
2024-06-02 12:21:43 -0500

    remove unnecessary sass usage, incorporate updates for variable DPRs

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', ()=>{
+
+  /*********************************************/
+  /*                                           */
+  /*           Globals and Constants           */
+  /*                                           */
+  /*********************************************/
   // set up constants
-  const _w = window.innerWidth;
-  const _h = window.innerHeight;
+  const _w = window.innerWidth * window.devicePixelRatio;
+  const _h = window.innerHeight * window.devicePixelRatio;
   const _l = Math.min(_w, _h);  // side length, the smaller of window width/height
 
   // set up canvas
@@ -9,39 +15,56 @@ window.addEventListener('load', ()=>{
   const ctx = c.getContext('2d');
   c.width = c.height = _l;
 
+  // set canvas size to always use the lower of either width or height as the constraining dimension
+  let cRect = c.getBoundingClientRect();
+  let cWidth = Math.abs(cRect.right - cRect.left);
+  let cHeight = Math.abs(cRect.bottom - cRect.top);
+  c.style.width = `${Math.min(cWidth, cHeight)}px`;
+  c.style.height = `${Math.min(cWidth, cHeight)}px`;
 
+  /*********************************************/
+  /*                                           */
+  /*           Global Lissa Settings           */
+  /*                                           */
+  /*********************************************/
   // set up global lissa settings
-  let lissaNum = 5 + (Math.round(_l / 100));   // total number of lissajous curves to show
+  let lissaNum = 5 + (Math.round(_l / 100)) / window.devicePixelRatio;   // total number of lissajous curves to show
   let sizeRatio = (lissaNum / 16); // this ratio will affect the size of the grid, animation speed, and dot size depending on screen real estate
   let fadeIntensity = 0;  // between 0 and 1, incl. Closer to 0 = more ghosting. Closer to 1 = more fading.
-  let lissaDotSize = 1.25 * sizeRatio; // the size of the dot tracing each curve
-  let lissaPadding = 6;   // padding between each curve
+  let lissaDotSize = 1.25 * sizeRatio * window.devicePixelRatio; // the size of the dot tracing each curve
+  let lissaPadding = 6 * window.devicePixelRatio;   // padding between each curve
 
   /* these should be left alone, since their values are derived */
   let lissaSize = _l / lissaNum;
-  let lissaDi = lissaSize - lissaPadding;
-  let lissaRad = lissaDi / 2;
+  let lissaDiameter = lissaSize - lissaPadding;
+  let lissaRadius = lissaDiameter / 2;
   let lissaTimer = 0;
   let lissas = [];
   let updateSpeed = (Math.PI / 200) / sizeRatio;    // the relative distance through the curve each point should advance per frame
-  // updateSpeed is also adjusted in the animation loop based on the time between requestAnimationFrame calls,
-  // which equalizes animation speed across multiple device frame rates
+  // updateSpeed is also adjusted in the animation loop based on the time between requestAnimationFrame calls, normalizing animation speed for variable frame rates
 
-  // set up lissa prototype
+  let ghostingFading = false;   // controls whether the patterns are solid or currently fading during each loop
+
+  /************************************/
+  /*                                  */
+  /*            Lissa Class           */
+  /*                                  */
+  /************************************/
+  // set up lissa class
   class Lissa {
     constructor(xFactor, yFactor) {
       this.xFactor = xFactor;
       this.yFactor = yFactor;
       // the center of each lissa
       this.origin = {
-        x: ((xFactor) * lissaSize) + lissaRad,
-        y: ((yFactor) * lissaSize) + lissaRad
+        x: ((xFactor) * lissaSize) + lissaRadius,
+        y: ((yFactor) * lissaSize) + lissaRadius
       };
       // the dot that will track the current position in the pattern
       this.dot = {
-        x: this.origin.x + lissaRad,
+        x: this.origin.x + lissaRadius,
         y: this.origin.y,
-        lastX: this.origin.x + lissaRad,
+        lastX: this.origin.x + lissaRadius,
         lastY: this.origin.y
       };
       // color variables
@@ -50,13 +73,15 @@ window.addEventListener('load', ()=>{
       this.bComp = 100 + (155 / (c.height / this.origin.y));
       this.color = 'rgb(' + this.rComp + ',' + this.gComp + ',' + this.bComp + ')';
     }
+
     // update the dot on each iteration
     updateDot() {
       this.dot.lastX = this.dot.x;
       this.dot.lastY = this.dot.y;
-      this.dot.x = (lissaRad * Math.cos(lissaTimer * this.xFactor)) + this.origin.x;
-      this.dot.y = (lissaRad * Math.sin(lissaTimer * this.yFactor)) + this.origin.y;
+      this.dot.x = (lissaRadius * Math.cos(lissaTimer * this.xFactor)) + this.origin.x;
+      this.dot.y = (lissaRadius * Math.sin(lissaTimer * this.yFactor)) + this.origin.y;
     }
+
     // draw using the dot.lastX/Y and dot.x/y props
     draw() {
       let currentLissa = this;
@@ -75,6 +100,12 @@ window.addEventListener('load', ()=>{
     }
   }
 
+  
+  /************************************************/
+  /*                                              */
+  /*            Functions and Listeners           */
+  /*                                              */
+  /************************************************/
   // clears the frame to a specified level of opacity, a float between 0 and 1, inclusive
   function clearFrame(opacity) {
     if (!isNaN(opacity)) {  // checking if the opacity variable is a number; because, why not?
@@ -85,8 +116,26 @@ window.addEventListener('load', ()=>{
     ctx.fillRect(0, 0, _l, _l);
   };
 
-  let ghostingFading = false;
+  // initialization function
+  function init() {
+    // populate the lissas array with lissas
+    for (let lissaX = 0; lissaX < lissaNum; lissaX++) {
+      for (let lissaY = 0; lissaY < lissaNum; lissaY++) {
+        let l = new Lissa(lissaX, lissaY);
+        lissas.push(l);
+      }
+    }
+    window.requestAnimationFrame(animate);
+  };
 
+  // the only listener needs to reload the entire animation on resize
+  window.addEventListener('resize', ()=>{ window.location.reload(); })
+    
+  /**********************************/
+  /*                                */
+  /*            Animation           */
+  /*                                */
+  /**********************************/
   // these variables will adjust each curve's movement speed to match the frame rate of the device (the time between rAF calls)
   let firstFrameTime = performance.now();
   let refreshThrottle = 1;
@@ -96,7 +145,7 @@ window.addEventListener('load', ()=>{
     // target 30fps by dividing the time between rAF callbacks by 30 to calculate per-frame movement
     tempRefreshThrottle = callbackTime - firstFrameTime;
     firstFrameTime = callbackTime;
-    refreshThrottle = tempRefreshThrottle / 30;
+    refreshThrottle = Math.min(1, tempRefreshThrottle / 30);
     
     // reset the timer to 0 once a rotation of 2pi is complete
     if (lissaTimer >= 2 * Math.PI) {
@@ -120,18 +169,6 @@ window.addEventListener('load', ()=>{
       lissa.draw();
     })
     window.requestAnimationFrame(animate);
-  };
-
-  // initialization function
-  function init() {
-    // populate the lissas array with lissas
-    for (let lissaX = 0; lissaX < lissaNum; lissaX++) {
-      for (let lissaY = 0; lissaY < lissaNum; lissaY++) {
-        let l = new Lissa(lissaX, lissaY);
-        lissas.push(l);
-      }
-    }
-    animate(0);
   };
 
   // begin!
